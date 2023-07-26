86586645b2b335b570d377b5b65b56e6942dc25c
SATTU3108 <127048911+SATTU3108@users.noreply.github.com>
2023-07-26 23:11:45 +0530

    Update index.js

--- Components/index.js
+++ Components/index.js
@@ -1,7 +1,9 @@
 import NavBar from "./NavBar/NavBar";
 import Filter from "./Filter/Filter";
+import Error from "./Error/Error";
+import Loader from "./Loader/Loader";
 import Model from "./Model/Model";
 import UserCard from "./UserCard/UserCard";
 import Friend from "./Friend/Friend";
 
-export { NavBar, Filter, Model, UserCard, Friend };
+export { NavBar, Filter, Error, Loader, Model, UserCard, Friend };
